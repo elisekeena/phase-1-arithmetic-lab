fea112a4c1ef4a45963c8e6e116b424362ab98aa
Elise <elise041294@gmail.com>
2022-04-05 09:26:20 -0600

    commit tree 27bb1f96ac4e77dd5d3ebca912c9165daf75e54a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+// Write your code here\
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 9
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,20);
